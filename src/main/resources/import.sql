224f9ebb3d75c38d1d5ed9cd0c43007000391cc4
Cléber da Silveira <clebers@dbserver.com.br>
2018-09-26 11:16:11 -0300

    Melhorias

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -12,3 +12,70 @@ insert into pergunta (nome) values ('Possui cultura de merge request.')
 insert into pergunta (nome) values ('Possui cultura de desenvolvimento em TDD.')
 insert into pergunta (nome) values ('Possui cultura de desenvolvimento por comportamento BDD.')
 insert into pergunta (nome) values ('As etapas de execução do pipeline é provisionada como código.')
+insert into pergunta (nome) values ('Possui template de identação, formatação e estilo de código.')
+insert into pergunta (nome) values ('O provisionamtuento dos ambientes computacionais é versionado como infraestrutura como código.')
+
+insert into pergunta (nome) values ('Possui pipeline automatizado.')
+insert into pergunta (nome) values ('Possui ferramenta de Quality Gate.')
+insert into pergunta (nome) values ('Possui métricas  bloqueantes de pipeline baseado em Quality Gate.')
+insert into pergunta (nome) values ('Possui métricas de pipeline não baseadas em Quality Gate.')
+insert into pergunta (nome) values ('Guarda métricas de execução de pipelines.')
+
+insert into pergunta (nome) values ('Efetua testes de unidades nos componentes.')
+insert into pergunta (nome) values ('Efetua testes de integração entre componentes.')
+insert into pergunta (nome) values ('Efetua testes de contratos (no caso de aplicações de protocolo web).')
+
+insert into pergunta (nome) values ('Efetua testes e2e.')
+insert into pergunta (nome) values ('Efetua testes de estresse.')
+insert into pergunta (nome) values ('Efetua testes de aceitação.')
+
+insert into pergunta (nome) values ('Possui teste de penetração ou vulnerabilidade.')
+
+insert into pergunta (nome) values ('Cria tags para releases.')
+
+insert into pergunta (nome) values ('Pratica Continuous Integration.')
+
+insert into pergunta (nome) values ('Pratica Continuous Delivery.')
+
+insert into pergunta (nome) values ('Pratica Continuous Deployment.')
+
+insert into pergunta (nome) values ('Possui ferramenta de Continuous Database Migration.')
+
+insert into pergunta (nome) values ('Versiona os artefatos de software binários ou imagens docker em gerenciador de artefatos.')
+
+insert into pergunta (nome) values ('Cria CHANGELOG com as informações de cada release.')
+
+insert into pergunta (nome) values ('Possui Blue-Green Deployment.')
+
+insert into pergunta (nome) values ('Possui Canary Release.')
+
+insert into pergunta (nome) values ('Possui A/B Testing.')
+
+insert into pergunta (nome) values ('Possui ambientes de teste e stage com semelhanças ao ambiente de produção.')
+
+insert into pergunta (nome) values ('Possui desenho, nem que mínimo, de seus componentes de software.')
+
+insert into pergunta (nome) values ('O time do projeto realiza sessões de design antes dos desenvolvimentos das features.')
+
+insert into pergunta (nome) values ('O time realiza discussões periódicas com a equipe de plataforma.')
+
+insert into pergunta (nome) values ('Possui canal ou ferramenta de comunicação do time.')
+
+insert into pergunta (nome) values ('Possui mural ou wiki do projeto contendo material de apoio para o time.')
+
+insert into pergunta (nome) values ('Possui definições e/ou planejamento de ASRs - Requisitos arquitetonicamente significativos')
+
+insert into pergunta (nome) values ('Possui a filosofia de SRE (Engenharia de confiabilidade do site)')
+
+insert into pergunta (nome) values ('Possui ferramenta de monitoramento da aplicação sendo executada em produção.')
+
+insert into pergunta (nome) values ('Possui testes de performance para melhoria de desempenho.')
+
+insert into pergunta (nome) values ('Coleta dados de navegação ou experiência para melhoria de fluxos visuais.')
+
+insert into pergunta (nome) values ('Possui ferramenta para visualização e/ou análise de logs.')
+
+insert into pergunta (nome) values ('Persiste logs por determinada janela temporal para posterior análise ou correlação.')
+
+insert into pergunta (nome) values ('Persiste métricas de softwares por determinada janela temporal para posterior análise ou correlação.')
+
